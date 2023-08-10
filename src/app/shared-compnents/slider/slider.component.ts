7b32aaec39622ae4c6bbae79128198e84a4c8f46
crazyoll <lag2@o2.pl>
2023-08-10 23:53:02 +0200

    Slider tweaks

--- src/app/shared-compnents/slider/slider.component.ts
+++ src/app/shared-compnents/slider/slider.component.ts
@@ -8,7 +8,7 @@ import { Component, Input } from '@angular/core';
 export class SliderComponent {
   @Input() minValue: number = 0;
   @Input() maxValue: number = 100;
-  protected currentValue: number = this.minValue;
+  protected currentValue: number = 50; //this.minValue;
 
   changeValue(event: Event){
     this.currentValue = (event.target as HTMLInputElement).valueAsNumber;
